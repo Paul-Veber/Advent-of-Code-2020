c3baad17ac51acf4e07cca726fef683d5562b1c7
Paul Veber <paulveber56@gmail.com>
2020-12-03 15:05:25 +0100

    solution for day 3

---
+++ day3/JS/day3.js
@@ -0,0 +1,42 @@
+import { array } from "./test.js"
+
+const resetPosition = (input, index) => {
+    if (!input[index]) {
+        const length = input.length - 1
+        const newPosition = index - length - 1
+        return newPosition
+    } else return index
+}
+
+const checkThree = (iteration, iterationJump, index, indexJump, count) => {
+    const solution = count
+    console.log(`solution ${solution}`)
+    if (iteration === array.length) return solution
+
+    const currentIteration = iteration
+
+    const ligne = array[iteration]
+
+    const currentIndex = resetPosition(ligne, index)
+
+    const check = ligne[currentIndex]
+
+    check === "."
+        ? checkThree(
+              currentIteration + iterationJump,
+              iterationJump,
+              currentIndex + indexJump,
+              indexJump,
+              solution
+          )
+        : checkThree(
+              currentIteration + iterationJump,
+              iterationJump,
+              currentIndex + indexJump,
+              indexJump,
+              solution + 1
+          )
+}
+
+console.log(checkThree(0, 1, 0, 3, 0))
+
